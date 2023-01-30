6576b6442378922e7e7121d367227be808a0a163
nezrinhasanli <nezhasanli@mail.ru>
2023-01-30 19:27:07 +0400

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+ /*
+ GIT
+ Proqram teminati inkisafi zamani istifade olunan Version Control Systemdir.Git sayesinde proyektlerin addim addim
+ versiyalarinin kopyalarini alaraq daha sonra ehtiyacimiz olanda aldigimiz kopyalara rahatca qayida bilerik.
+ Github proyektimizi saxlamaq ucun bir yerdi.
+ Git proyekt versiyalarini idare etmek ucun bir vasitedir.
+
+ -git config, sayesinde komputerimizdeki git terminalini oz github hesabimiza baglayiriq
+ -git config –global user.name “istifadeci adi”
+-git config –global user.email “istifadeci e-mail adresiniz”
+-git init,komputerimizde yaratdigimiz faylin git'e elave edilmesi ucun istifade olunur.
+-git status,repository haqqinda hazirki bilgini verir
+-git clone,githubdaki repository'ni komputerimize copy edirik
+-git add .(stage merhelesi),proyektimizde elediyimiz deyisiklikleri repository'ye elave edir.commit etmeye hazir hala getirir.
+-git commit . -m "ad","git add" ile etdiyimiz deyisiklikleri save edir.
+-git push,proyektimizi githuba gonderir.
+-git log, elediyimiz deyisiklikleri gosterir
+-cd,fayllar arasi kecid
+
+  */
